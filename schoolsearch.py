0de22f29d8ccdc1ef92c0e73dc3103954ef84098
colegrigsby <cole.grigsby@gmail.com>
2017-04-04 11:13:14 -0700

    Added some test cases and started cleaning up code. brb

--- schoolsearch.py
+++ schoolsearch.py
@@ -14,89 +14,131 @@ class Student:
         self.tlastname = in_tlastname
         self.tfirstname = in_tfirstname
 
-#empty list to store each student
-studentList = []
 
-#read through students.txt
-inputFile = open("students.txt", 'r')
-while True:
-    line = inputFile.readline()
-    if len(line) == 0:
-        break
-    data = line.split(',')
-    #add data to list
-    studentList.append(Student(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7]))
-
+# Traceability: implements requirements R3, R4
 #function to search for student last name
+
 def studentLastName(results, students, inArr):
     for i in range(0, len(students)):
         if students[i].stlastname == inArr[1]:
             results.append(students[i])
 
+# Traceability: implements requirements R3, R6
+
 #function to search for teacher last name
 def teacherLastName(results, students, inArr):
-    for i in range(0, len(students)):
-        if students[i].tlastname == inArr[1]:
-            results.append(students[i])
+    for s in students:
+        if s.tlastname == inArr[1]:
+            results.append(s)
+
+# Traceability: implements requirements R3, R7
 
 #function to search for same grade (0-6)
 def gradeNumber(results, students, inArr):
-    for i in range(0, len(students)):
-        if students[i].grade == inArr[1]:
-            results.append(students[i])
+    for s in students:
+        if s.grade == inArr[1]:
+            results.append(s)
+
+
+# Traceability: implements requirements R3, R8
 
 #function to search for same bus route
 def busRoute(results, students, inArr):
-    for i in range(0, len(students)):
-        if students[i].bus == inArr[1]:
-            results.append(students[i])
+    for s in students:
+        if s.bus == inArr[1]:
+            results.append(s)
+
+# Traceability: implements requirements R3, R9
+
+#function to search for the highest student in a grade 
+def gradeTop(results, students, inArr): 
+	r = gradeNumber(results, students, inArr)
+	max = students[0] 
+	for s in students: 
+		if s.gpa > max.gpa: 
+			max = s	
+	results = [max]
+
+# Traceability: implements requirements R3, R9
+
+#function to search for the lowest student GPA in a grade 
+def gradeTop(results, students, inArr):
+    r = gradeNumber(results, students, inArr)
+    min = students[0]
+    for s in students:
+        if s.gpa < min.gpa:
+            min = s
+    results = [min]
+
+
+
 
 #get user input commands
-while True:
-    userInput = raw_input("Please Enter Command: ")
-
-    inputArray = userInput.split(' ')
-
-    resultsArray = []
-    if len(inputArray) == 1:
-        #R12 Q[uit]
-        #quit the current session
-        if inputArray[0] == "Q" or inputArray[0] == "Quit":
+def runProg():
+    #empty list to store each student
+    studentList = []
+    #read through students.txt
+    inputFile = open("students.txt", 'r')
+    while True:
+        line = inputFile.readline()
+        if len(line) == 0:
             break
+        data = line.split(',')
+        #add data to list
+        studentList.append(Student(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7]))
+    
+    #run program inputs R1 R2
+    while True:
+        userInput = raw_input("Please Enter Command: ")
 
-    if len(inputArray) == 2:
-        #R4 S[tudent]: <lastname> 
-        #check if input is for student lastname
-        if inputArray[0] == "S:" or inputArray[0] == "Student:":
-            studentLastName(resultsArray, studentList, inputArray)
-            for i in range(0, len(resultsArray)):
-                print resultsArray[i].stlastname + "," + resultsArray[i].stfirstname + "," + resultsArray[i].grade + "," + resultsArray[i].room + "," + resultsArray[i].tlastname + "," + resultsArray[i].tfirstname,
-        #R6 T[eacher]: <lastname>
-        #check if input is for teacher lastname
-        elif inputArray[0] == "T:" or inputArray[0] == "Teacher:":
-            teacherLastName(resultsArray, studentList, inputArray)
-            for i in range(0, len(resultsArray)):
-                print resultsArray[i].stlastname + "," + resultsArray[i].stfirstname
-        #R7 G[rade]: <Number>
-        #check if input is for grade number
-        elif inputArray[0] == "G:" or inputArray[0] == "Grade:":
-            gradeNumber(resultsArray, studentList, inputArray)
-            for i in range(0, len(resultsArray)):
-                print resultsArray[i].stlastname + "," + resultsArray[i].stfirstname
-        #R8 B[us]: <Number>
-        elif inputArray[0] == "B:" or inputArray[0] == "Bus:":
-            busRoute(resultsArray, studentList, inputArray)
-            for i in range(0, len(resultsArray)):
-                print resultsArray[i].stlastname + "," + resultsArray[i].stfirstname + "," + resultsArray[i].grade + "," + resultsArray[i].room
-                
-    #R5 S[tudent]: <lastname> [B[us]]
-    #check if input is for student lastname and bus route
-    elif len(inputArray) == 3:
-        if inputArray[0] == "S:" or inputArray[0] == "Student:":
-            if inputArray[2] == "B" or inputArray[2] == "Bus":
-                studentLastName(resultsArray, studentList, inputArray) 
-                for i in range(0, len(resultsArray)):
-                    print resultsArray[i].stlastname + "," + resultsArray[i].stfirstname + "," + resultsArray[i].bus,
+        inputArray = userInput.split(' ')
+        
+        if len(inputArray) == 1:
+            #R11 [info]
+            if inputArray[0] == "I" or inputArray[0] == "Info":
+                break
+                #TODO
+            
+            #R12 Q[uit]
+            #quit the current session
+            if inputArray[0] == "Q" or inputArray[0] == "Quit":
+                break
+    
+    
+        resultsArray = []
+        if len(inputArray) == 2:
+            #R4 S[tudent]: <lastname> 
+            #check if input is for student lastname
+            if inputArray[0] == "S:" or inputArray[0] == "Student:":
+                studentLastName(resultsArray, studentList, inputArray)
+                for r in resultsArray:
+                    print r.stlastname + "," + r.stfirstname + "," + r.grade + "," + r.room + "," + r.tlastname + "," + r.tfirstname,
+            #R6 T[eacher]: <lastname>
+            #check if input is for teacher lastname
+            elif inputArray[0] == "T:" or inputArray[0] == "Teacher:":
+                teacherLastName(resultsArray, studentList, inputArray)
+                for r in resultsArray:
+                    print r.stlastname + "," + r.stfirstname
+            #R7 G[rade]: <Number>
+            #check if input is for grade number
+            elif inputArray[0] == "G:" or inputArray[0] == "Grade:":
+                gradeNumber(resultsArray, studentList, inputArray)
+                for r in resultsArray:
+                    print r.stlastname + "," + r.stfirstname
+            #R8 B[us]: <Number>
+            elif inputArray[0] == "B:" or inputArray[0] == "Bus:":
+                busRoute(resultsArray, studentList, inputArray)
+                for r in resultsArray:
+                    print r.stlastname + "," + r.stfirstname + "," + r.grade + "," + r.room
+                    
+        #R5 S[tudent]: <lastname> [B[us]]
+        #check if input is for student lastname and bus route
+        elif len(inputArray) == 3:
+            if inputArray[0] == "S:" or inputArray[0] == "Student:":
+                if inputArray[2] == "B" or inputArray[2] == "Bus":
+                    studentLastName(resultsArray, studentList, inputArray) 
+                    for r in resultsArray:
+                        print r.stlastname + "," + r.stfirstname + "," + r.bus,
 
-
+runProg()
 inputFile.close()
